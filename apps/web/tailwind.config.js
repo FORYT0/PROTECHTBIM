388de5106f3129b178ac361c81af52cee1e2688d
Pete-rise <abi.nyamatu@gmail.com>
2026-03-21 17:18:51 +0300

    fix(web): Restore absolute paths to Tailwind to fix Vercel CSS generation

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, 'index.html'),
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
     "../../apps/web/index.html",
     "../../apps/web/src/**/*.{js,ts,jsx,tsx}",
   ],
